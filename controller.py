6c18ab074eee90632fc4e2d10fe39fb9fa4448af
Alejandro Guerrero <guerrero.alejandro@gmail.com>
2023-05-21 16:43:14 -0300

    eliminación file Program.py

--- controller.py
+++ controller.py
@@ -7,25 +7,27 @@ from view import View
 class Controller:
     # Constructor - Inicialización del Objeto instanciado
     def __init__(self):
-        #print("init Controller")
+        # print("init Controller")
         self.model = Model(self)
         self.view = View(self)
 
     def main(self):
-        #print("main Controller")
+        # print("main Controller")
         self.view.main()
         self.model.main()
 
     def decargarPartidosPoliticos(self, info):
-        #print("decargarPartidosPoliticos Controller")
+        # print("decargarPartidosPoliticos Controller")
         self.model.writePartidosPoliticos(info)
 
     def decargarRegionesGeograficas(self, info):
-        #print("decargarRegionesGeograficas Controller")
+        # print("decargarRegionesGeograficas Controller")
         self.model.writeRegionesGeograficas(info)
 
 
 # INICIO DE LA APP
 if __name__ == '__main__':
+    # Instanciamos la clase Controller
     app = Controller()
+# Invocamos al método main de la clase Controller
     app.main()
